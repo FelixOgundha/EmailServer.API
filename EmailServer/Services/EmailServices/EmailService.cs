27b23b18b2be395aad5a4ca706ce0375ec3ba84e
FelixOgundha <fogundha@strathmore.edu>
2022-09-27 22:35:05 +0300

    Add project files.

---
+++ EmailServer/Services/EmailServices/EmailService.cs
@@ -0,0 +1,24 @@
+﻿using EmailServer.Models;
+
+namespace EmailServer.Services.EmailServices
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(Email email)
+        {
+            /* // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from ?? _appSettings.EmailFrom));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(_appSettings.SmtpHost, _appSettings.SmtpPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_appSettings.SmtpUser, _appSettings.SmtpPass);
+            smtp.Send(email);
+            smtp.Disconnect(true);8*/
+        }
+    }
+}
---
+++ EmailServer/Services/EmailServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using EmailServer.Models;
+
+namespace EmailServer.Services.EmailServices
+{
+    public interface IEmailService
+    {
+        public void SendEmail(Email email);
+    }
+}
